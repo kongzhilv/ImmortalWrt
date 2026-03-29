0f74ef704c4926a31cad90831e23b0ab911aab89
kongzhilv <157718507+kongzhilv@users.noreply.github.com>
2026-03-29 18:16:14 +0800

    Create diy-part1.sh for software source management
    Add a script for pre-compilation to manage software sources.

---
+++ diy-part1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# 描述: 编译前执行，用于添加软件源
+
+# 🚨 警告：ImmortalWrt 已经原生自带 OpenClash 等所有国内环境插件！
+# 绝对不要在这里添加 kenzo 等第三方杂交源，否则 100% 导致文件冲突和 Error 255 报错！
+
+echo "DIY Part 1: 使用 ImmortalWrt 纯净原生环境，不添加第三方源。"
