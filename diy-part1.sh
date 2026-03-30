d7b5183bdb768ef03102543a006505a1a33ea910
kongzhilv <157718507+kongzhilv@users.noreply.github.com>
2026-03-30 23:41:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-# 描述: 编译前执行，用于添加软件源
+# 描述: 编译前执行，用于添加第三方软件源
 
-# 🚨 警告：ImmortalWrt 已经原生自带 OpenClash 等所有国内环境插件！
-# 绝对不要在这里添加 kenzo 等第三方杂交源，否则 100% 导致文件冲突和 Error 255 报错！
-
-echo "DIY Part 1: 使用 ImmortalWrt 纯净原生环境，不添加第三方源。"
+# 添加 kenzok8 维护的国内精选插件源 
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
