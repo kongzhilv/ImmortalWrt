19aabb0250bddecff5875eb5c62d8b16365d92bc
kongzhilv <157718507+kongzhilv@users.noreply.github.com>
2026-03-31 11:20:38 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -2,5 +2,5 @@
 # 描述: 编译前执行，用于添加第三方软件源
 
 # 添加 kenzok8 维护的国内精选插件源 
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+#echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+#echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
